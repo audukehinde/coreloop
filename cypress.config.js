e48af8c7e8d8fedede0665b0df0f7864302b2593
KennyrepQA <kehinde@almondcarrers.com>
2026-06-24 00:48:20 +0100

    Chore: Code refactor and added yml file for CICD

--- cypress.config.js
+++ cypress.config.js
@@ -3,10 +3,32 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   allowCypressEnv: false,
 
+  reporter: 'mochawesome',
+
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: true,
+    json: true,
+  },
+
   e2e: {
     // implement node event listeners here
     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
     baseUrl: "https://coreloops-v2-350156031212.europe-west2.run.app/auth/login"
   },
+  viewportWidth: 1440,
+  viewportHeight: 900,
 
+  video: true,
+
+  screenshotOnRunFailure: true,
+
+  defaultCommandTimeout: 10000,
+
+  pageLoadTimeout: 30000,
+
+  requestTimeout: 15000,
+
+  responseTimeout: 15000,
 });
